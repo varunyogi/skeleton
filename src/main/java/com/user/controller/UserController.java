811463821c7236104f0d3a5f0794501502c84ec5
Varun Kaushik <varun.yogi@gmail.com>
2023-11-24 00:48:02 +0100

    adding base package

---
+++ src/main/java/com/user/controller/UserController.java
@@ -0,0 +1,16 @@
+package com.user.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    @GetMapping("/list")
+    public String getValue(){
+
+        return "Hey Varun";
+    }
+}
